cbdbfcf8b40b61792e316a408da66e3a05bdb30c
AbdelkaderBelhadj-code <belhajabdelkader3@gmail.com>
2023-09-28 22:37:08 +0100

    IT IS WORKING

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,7 +24,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //hello world
+        //it is now working
         $posts = Post::all(); // Retrieve all post
         return view('home', compact('posts'));
         //return view("/");
